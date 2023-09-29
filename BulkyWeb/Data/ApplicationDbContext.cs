c7db7ecf8edb6dfcf68966ab2939a4eb7547a6b8
vlad.romanyuk <prostovlad871@gmail.com>
2023-09-29 19:04:09 +0300

    Add Category Table to DB

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data {
     public class ApplicationDbContext : DbContext {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {   
         }
-    
-        
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
