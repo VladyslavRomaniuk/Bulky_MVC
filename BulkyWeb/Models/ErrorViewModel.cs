5638ff396e6ee336cc88657a38398f65be818dfc
vlad.romanyuk <prostovlad871@gmail.com>
2023-09-29 18:15:56 +0300

    Brackets update

--- BulkyWeb/Models/ErrorViewModel.cs
+++ BulkyWeb/Models/ErrorViewModel.cs
@@ -1,7 +1,5 @@
-namespace BulkyWeb.Models
-{
-    public class ErrorViewModel
-    {
+namespace BulkyWeb.Models {
+    public class ErrorViewModel {
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
