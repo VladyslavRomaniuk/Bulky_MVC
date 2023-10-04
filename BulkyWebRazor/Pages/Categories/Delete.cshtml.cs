c785c96b2931ee32fbe6b15fbe75b2f59c90a356
vlad.romanyuk <prostovlad871@gmail.com>
2023-10-04 18:43:34 +0300

    Implemented CRUD operations for Category Table

---
+++ BulkyWebRazor/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,32 @@
+using BulkyWebRazor.Data;
+using BulkyWebRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category? Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db) {
+            _db = db;
+        }
+
+        public void OnGet(int? id) {
+            if (id != null && id != 0) {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost() {
+            _db.Categories.Remove(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully!";
+
+            return RedirectToPage("Index");
+        }
+    }
+}
