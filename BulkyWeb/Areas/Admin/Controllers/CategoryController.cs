4b9b3a2899eb74151e37ec5bbe75a763896b4123
vlad.romanyuk <prostovlad871@gmail.com>
2023-10-07 18:56:29 +0300

    Implemented Areas separating (Admin and Customer)

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -4,10 +4,11 @@ using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyWeb.Controllers {
+namespace BulkyWeb.Areas.Admin.Controllers {
+    [Area("Admin")]
     public class CategoryController : Controller {
         private readonly IUnitOfWork _unitOfWork;
-        
+
         public CategoryController(IUnitOfWork unitOfWork) {
             _unitOfWork = unitOfWork;
         }
